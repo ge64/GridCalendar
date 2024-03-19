922a339cd922f507ad7ae69267d17f93064839e3
Gabriel Eaton <50244998+ge64@users.noreply.github.com>
2024-03-18 23:50:59 -0400

    Initial commit

---
+++ src/gridTest1.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Grid, GridItem, Box, Image, Text, Badge, Button } from '@chakra-ui/react';
+
+
+
+export default EventGrid;
---
+++ src/stories/gridTest1.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import MyCalendar from '../chatty-calendar/src/MyCalendar';
+
+// import { demoEvents } from '../chatty-calendar/src/exampleEvent';
+// import demoEvents from '../chatty-calendar/src/exampleEvent.json';
+
+export default {
+    title: 'Grid Test 1',
+    component: MyCalendar,
+};
+
+// export const Default = () => <MyCalendar events={demoEvents} />;
